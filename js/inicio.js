4fc29b12813c3b239cb1028feb5cb3e35ec05402
handrya0809-spec <handrya0809@gmail.com>
2025-12-09 18:11:21 -0500

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -8,12 +8,12 @@ onAuthStateChanged(auth, (user) => {
     const userEmail = document.getElementById("user-email");
 
     if (user) {
-        // Si está logueado, mostrar correo y botón de logout
+        // Si el usuario está logueado, muestra el correo y el botón "Cerrar sesión"
         if (userEmail) userEmail.textContent = user.email;
         if (logoutButton) logoutButton.style.display = "block";
-        if (loginButton) loginButton.style.display = "none"; // Ocultar el botón de login cuando el usuario está logueado
+        if (loginButton) loginButton.style.display = "none"; // Ocultar "Iniciar sesión"
     } else {
-        // Si no está logueado, mostrar solo el botón de login
+        // Si no está logueado, muestra el botón "Iniciar sesión"
         if (logoutButton) logoutButton.style.display = "none";
         if (loginButton) loginButton.style.display = "block";
     }
@@ -24,7 +24,7 @@ const logoutBtn = document.getElementById("logoutBtn");
 if (logoutBtn) {
     logoutBtn.addEventListener("click", () => {
         signOut(auth).then(() => {
-            window.location.href = "index.html"; // Redirigir al login
+            window.location.href = "login-cliente.html"; // Redirigir al login de cliente
         });
     });
 }
