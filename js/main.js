78d8ef4a6e04146e12c418ebf4b3a1b2acfd0b6c
handrya0809-spec <handrya0809@gmail.com>
2025-12-10 16:16:16 -0500

    Refactor Firebase imports and initialization

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-// Importar Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+// js/main.js
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js';
 
-// CONFIGURACIÓN DE TU PROYECTO
+
 const firebaseConfig = {
-    apiKey: "AIzaSyC8fNiiE294HA6fmo3hX8AZRX8lboH8YuI",
-    authDomain: "reserva-hoteles-e2e6f.firebaseapp.com",
-    projectId: "reserva-hoteles-e2e6f",
-    storageBucket: "reserva-hoteles-e2e6f.firebasestorage.app",
-    messagingSenderId: "566328480066",
-    appId: "1:566328480066:web:9e98afc4ce144ead2d78db",
-    measurementId: "G-ZLJXZ8JZKW"
+apiKey: "AIzaSyC8fNiiE294HA6fmo3hX8AZRX8lboH8YuI",
+authDomain: "reserva-hoteles-e2e6f.firebaseapp.com",
+projectId: "reserva-hoteles-e2e6f",
+storageBucket: "reserva-hoteles-e2e6f.firebasestorage.app",
+messagingSenderId: "566328480066",
+appId: "1:566328480066:web:9e98afc4ce144ead2d78db",
+measurementId: "G-ZLJXZ8JZKW"
 };
 
-// Inicialización
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// Exportar para usar en otros módulos
+
 export { db, auth };
